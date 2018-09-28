210806dd587e96bca1f42b3169ea655a8de4aa1a
TechQuery <shiy007@qq.com>
2018-09-28 17:25:32 +0800

    [ Add ]  First prototype version

---
+++ source/DataScheme.js
@@ -0,0 +1,69 @@
+/**
+ * @param {Object} meta - Key for Name & Value for `Function` / `RegExp`
+ *
+ * @return {(Class: Function): void} Class decorator for Data Scheme
+ */
+export function schemeOf(meta) {
+
+    return  Class => {
+
+        Object.defineProperty(Class, 'meta', {
+            value:       meta,
+            enmuerable:  true
+        });
+
+        for (let key in meta) {
+
+            if (!(meta[key] instanceof Array))  meta[key] = [ meta[key] ];
+
+            Object.defineProperty(Class.prototype, key, {
+                set:         value => Class.set(key, value),
+                enmuerable:  true
+            });
+        }
+    };
+}
+
+@schemeOf({
+    id:    Number,
+    name:  String
+})
+export class DataScheme {
+    /**
+     * @param {Function|RegExp} scheme
+     * @param {*}               value
+     *
+     * @return {Boolean}
+     */
+    static check(scheme, value) {
+
+        if (scheme instanceof Function) {
+
+            if (global[scheme.name] === scheme) {
+
+                if (Object(value) instanceof scheme)  return true;
+            } else
+                return  scheme( value );
+
+        } else if (scheme instanceof RegExp)
+            return  scheme.test(value + '');
+
+        return false;
+    }
+
+    /**
+     * @param {String} key
+     * @param {*}      value
+     */
+    static set(key, value) {
+
+        const scheme = this.meta[ key ];
+
+        if (! scheme.some(item  =>  this.check(item, value)))
+            throw TypeError(
+                `"${key}" of ${this.name} should be a ${
+                    scheme.map(item  =>  (item.name || item + '')).join(' / ')
+                }`
+            );
+    }
+}
---
+++ test/DataScheme.js
@@ -0,0 +1,56 @@
+import { DataScheme, schemeOf } from '../source/DataScheme';
+
+
+describe('Data Scheme',  () => {
+    /**
+     * @test {DataScheme.check}
+     */
+    describe('Check schemes',  () => {
+
+        it(
+            'Global Type',
+            ()  =>  DataScheme.check(Number, 1).should.be.true()
+        );
+
+        it(
+            'Custom Callback',
+            ()  =>  DataScheme.check(value => !value,  0).should.be.true()
+        );
+
+        it(
+            'Regular Expression',
+            ()  =>  DataScheme.check(/test/, 'example').should.be.false()
+        );
+    });
+
+    /**
+     * @test {schemeOf}
+     */
+    describe('Decorate classes',  () => {
+
+        it('Base class',  () => {
+
+            const data = new DataScheme();
+
+            (() => data.id = '').should.throw(
+                '"id" of DataScheme should be a Number'
+            );
+        });
+
+        it('Implementation class',  () => {
+
+            @schemeOf({
+                email:  /.+?@(.+?\.){1,}\w+/,
+                phone:  /[0-9+-]{7,}/
+            })
+            class User extends DataScheme { }
+
+            const user = new User();
+
+            (()  =>  (user.name = 'TechQuery', user.email = '@gmail.com'))
+                .should.throw(
+                    '"email" of User should be a /.+?@(.+?\\.){1,}\\w+/'
+                );
+        });
+    });
+});
